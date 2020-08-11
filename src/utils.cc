11fc8497cba0d90a352121b5faf01c0cd4a94240
Connor Ward <cward.contrib@fastmail.com>
2020-08-11 16:18:57 +0100

    Fix units conversion error

--- src/utils.cc
+++ src/utils.cc
@@ -31,13 +31,13 @@ namespace perplexcpp
   {
     double convert_pascals_to_bar(const double pressure_in_pascals) 
     {
-      return pressure_in_pascals / 1e6;
+      return pressure_in_pascals / 1e5;
     }
 
 
     double convert_bar_to_pascals(const double pressure_in_bar) 
     {
-      return pressure_in_bar * 1e6;
+      return pressure_in_bar * 1e5;
     }
 
 
